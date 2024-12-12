805bbcf8dfd5fe35d759aafc8dceeef7559ed683
Manos Daskalelis <Manolisdaskalelis@gmai.com>
2024-12-12 21:38:42 +0200

    tickets and complains added

---
+++ BlueOceanApi/BlueOcean.Data/Models/Complain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueOcean.Data.Models
+{
+    public class Complain
+    {
+        public string TicketTitle { get; set; }
+        public string Reason { get; set; }
+        public string Description { get; set; }
+
+        public Guid UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
