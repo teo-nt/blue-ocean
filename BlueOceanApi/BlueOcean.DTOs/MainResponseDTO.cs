09600c44c2c131acec9e43babb78ea0cbcd9e492
Teo <thod_ntonas@hotmail.com>
2024-12-16 22:46:04 +0200

    Fix boat profile

--- BlueOceanApi/BlueOcean.DTOs/MainResponseDTO.cs
+++ BlueOceanApi/BlueOcean.DTOs/MainResponseDTO.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace BlueOcean.DTOs
 {
     public class MainResponseDTO
     {
-        public object Result {  get; set; }
-        public string Error { get; set; }
+        public object? Result {  get; set; }
+        public string? Error { get; set; }
     }
 }
--- BlueOceanApi/BlueOcean.Helpers/Mapper/BoatProfile.cs
+++ BlueOceanApi/BlueOcean.Helpers/Mapper/BoatProfile.cs
@@ -10,15 +10,10 @@ public class BoatProfile : Profile
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()))
             .ForMember(dest => dest.BoatCategory, opt => opt.MapFrom(src => (int)src.BoatCategory))
             .ForMember(dest => dest.Port, opt => opt.MapFrom(src => src.Location.Port))
-            .ForMember(dest => dest.Owner, opt => opt.MapFrom(src => new
-            {
-                src.Owner.Id,
-                FullName = $"{src.Owner.Firstname} {src.Owner.Lastname}"
-            }))
             .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Image));
 
         CreateMap<User, OwnerDTO>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()))
             .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.Firstname} {src.Lastname}"));
     }
 }
